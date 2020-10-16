4e796003afb74c1fef0a9aecc8b3dd8d5954bd5f
Icebob <mereg.norbert@gmail.com>
2020-10-16 20:31:36 +0200

    build

--- backend/services/www.service.js
+++ backend/services/www.service.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const Moleculer = require("moleculer");
 
 module.exports = {
 	name: "www",
+
+	mixins: [Moleculer.Mixins.ModuleConfigMixin],
 
 	started() {
 		this.server = express();
 
 		this.server.use("/media", express.static("./data/media"));
-		this.server.use(express.static("./frontend/public"));
+		this.server.use(express.static("./public"));
 
-		const port = 3000;
+		const port = process.env.PORT || this.config.httpPort;
 		this.server.listen(port, () => {
 			this.logger.info(`WWW service listening at http://localhost:${port}`);
 		});
