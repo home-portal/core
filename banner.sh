ac8bb1b9e3390dcbe7a081291fd45b6c9881e69a
Icebob <mereg.norbert@gmail.com>
2020-11-26 11:04:53 +0100

    banner, more info

--- banner.sh
+++ banner.sh
@@ -15,9 +15,10 @@ else
 fi
 
 printf "\n"
-printf "Welcome to Home Portal Device (on %s)!\n" "$(uname -a)"
-printf "Version: ${GREEN}${BOLD}${APP_VERSION}${NORMAL}"
-printf "\n\n\n"
+printf "Welcome to Home Portal Device!\n\n"
+printf "Version: ${GREEN}${BOLD}${APP_VERSION}${NORMAL}\n"
+printf "OS: %s\n" "$(uname -srv)"
+printf "\n"
 
 date=`date`
 load=`cat /proc/loadavg | awk '{print $1}'`
@@ -27,12 +28,14 @@ total_memory=`free -h | awk '/Mem\:/ { print $2 }'`
 time=`uptime | grep -ohe 'up .*' | sed 's/,/\ hours/g' | awk '{ printf $2" "$3 }'`
 processes=`ps aux | wc -l`
 ip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+hostname=`hostname`
 temp=`/opt/vc/bin/vcgencmd measure_temp | cut -d= -f2 | awk '{print $1}'`
 backend=`if [[ $(ps aux | grep home-portal | grep node | wc -l) > 0 ]] ; then echo "${GREEN}${BOLD}Running${NORMAL}"; else echo "${GREEN}${BOLD}Not running${NORMAL}"; fi`
 
 echo "System information:"
 echo "----------------------------------"
 printf "IP Address:\t${YELLOW}${BOLD}%s ${NORMAL}\n" $ip
+printf "Hostname:\t%s\n" $hostname
 printf "System load:\t%s\n" $load
 printf "CPU temp:\t%s\n" $temp
 printf "Memory usage:\t%s of %s\n" $memory_usage $total_memory
