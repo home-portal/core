0470fe729bdd589180101d72155029ddc1f74edf
Icebob <mereg.norbert@gmail.com>
2020-12-12 21:26:54 +0100

    calendar widget

--- modules/ui-events/frontend/index.js
+++ modules/ui-events/frontend/index.js
@@ -5,18 +5,19 @@ const Vue = HomePortal.dependencies.vue;
 import Widget from "./widgets/CalendarWidget.vue";
 
 HomePortal.registerWidget({
-	name: "calendar-google",
-	module: "calendar-google",
+	name: "events",
+	module: "ui-events",
 	mountDiv: el => new Vue({ render: h => h(Widget) }).$mount(el).$el
 });
-
+/*
 import Page from "./CalendarPage.vue";
 
 HomePortal.registerPage({
-	name: "calendar-google",
-	module: "calendar-google",
+	name: "events",
+	module: "ui-events",
 	showInQuickLaunch: true,
 	caption: "Calendar",
 	icon: "fa fa-calendar-alt",
 	mountDiv: el => new Vue({ render: h => h(Page) }).$mount(el).$el
 });
+*/
--- modules/ui-events/frontend/widgets/CalendarWidget.vue
+++ modules/ui-events/frontend/widgets/CalendarWidget.vue
@@ -1,5 +1,12 @@
 <template>
-	<current-info v-if="results" :data="results.now" :settings="settings" :show-location="true" class="h-full cursor-pointer" @click="goToWeather()" />
+	<div class="">
+		<ul>
+			<li v-for="event in upcomingEvents" :key="event.id">
+				<div class="time">{{ eventTime(event) }}</div>
+				<div class="title">{{ eventCaption(event) }}</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
@@ -7,38 +14,107 @@
 const _ = HomePortal.dependencies.lodash;
 const moment = HomePortal.dependencies.moment;
 
-import CurrentInfo from "../components/CurrentInfo.vue";
-
 export default {
 	components: {
-		CurrentInfo
+	},
+
+	computed: {
+		upcomingEvents() {
+			if (!this.data) return [];
+
+			let list = [];
+			Object.values(this.data).forEach(calItem => {
+				if (this.settings.widget.calendars && !this.settings.widget.calendars.includes(calItem.name)) return;
+				list.push(calItem.events);
+			});
+			list = _.flattenDeep(list);
+
+			// Skipping day events
+			if (this.settings.skipDayEvents)
+				list = list.filter(event => !event.isDayEvent);
+
+			list.sort((a, b) => a.startDate - b.startDate);
+
+			return list.slice(0, this.settings.widget.count);
+		},
 	},
 
 	data() {
 		return {
-			results: null
+			data: null
 		};
 	},
 
 	events: {
-		"weather.info.updated"(ctx) {
-			this.results = ctx.params;
+		"current.events.updated"(ctx) {
+			this.data = ctx.params;
 		}
 	},
 
 	methods: {
-		goToWeather() {
-			this.broker.call('$router.goTo', { page: 'weather' });
+		goToPage() {
+			//this.broker.call('$router.goTo', { page: 'events' });
+		},
+
+		eventTime(event) {
+			if (event.isDayEvent) {
+				const ds = moment(event.startDate);
+				const de = moment(event.endDate);
+
+				if (de.diff(ds, "days") > 1)
+					return ds.format("MMM D") + " - " + de.format("MMM D")
+				else
+					return ds.format("MMM D");
+
+			} else {
+				const d = moment(event.startDate);
+				if (d.isSame(moment()), "day")
+					return d.format("LT");
+				else
+					return d.format("MMM D") + " " + d.format("LT");
+			}
+		},
+
+		eventCaption(event) {
+			return _.truncate(event.title, { length: 35 });
 		}
 	},
 
 	created() {
-		this.settings = HomePortal.getModuleSettings("weather");
+		this.settings = HomePortal.getModuleSettings("ui-events");
 		//console.log("Module settings", this.settings);
 	},
 
 	async mounted() {
-		this.results = await this.broker.call("weather.get");
+		this.data = await this.broker.call("current.get", { key: "events" });
 	}
 };
 </script>
+
+<style lang="scss" scoped>
+ul {
+	padding: 0em;
+
+	li {
+		margin: 0.3em 0;
+		background-color: rgba(black, 0.5);
+		border-radius: 1em;
+
+		display: flex;
+
+		.time {
+			min-width: 5em;
+			text-align: right;
+			background-color: var(--bg1);
+			padding: 0.1em 0.5em;
+			border-radius: 1em 0 0 1em;
+		}
+
+		.title {
+			flex: 1;
+			padding: 0.1em 0.5em;
+		}
+	}
+}
+
+</style>
